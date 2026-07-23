314dfd9876d38c53f4d73069010245c6ea1c5d2a
Duane Northcutt <duane.northcutt@gmail.com>
2026-07-22 18:20:10 -0700

    Created.

---
+++ bin/nb15.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Script to run Jupyter Notebook 15
+#
+# To create notebook:
+#  jupyter nbconvert --to notebook --execute notebooks/15_uma16_array.ipynb --output notebooks/15_uma16_array.ipynb
+
+jupyter lab notebooks/15_uma16_array.ipynb
